2f9e15096f1fdcaa69ecd792ff570a2bd4752826
Akinjide Bankole <r@akinjide.me>
2017-06-22 15:24:30 +0100

    feat(conf): update with process env

--- config/env.js
+++ config/env.js
@@ -10,18 +10,18 @@
  * REDIS
  *
  *
- * server hostname
- * server port number
- * server password
- * db index
+ * server hostname > redis-host
+ * server port number > redis-port
+ * server password > redis-pass
+ * db index > redis-db
  *
  **/
 
 module.exports = {
-  'url': 'http://127.0.0.1:3000',
-  'port': 3000
-//   'redis-host':
-//   'redis-port':
-//   'redis-pass':
-//   'redis-db':
+  'url': process.env.URL || 'http://127.0.0.1:3000',
+  'port': process.env.PORT || 3000,
+  'redis-host': process.env.REDIS_HOSTNAME || 'localhost',
+  'redis-port': process.env.REDIS_PORT || 6379,
+  'redis-pass': process.env.REDIS_PASSWORD || false,
+  'redis-db': process.env.REDIS_DB || 0
 }
